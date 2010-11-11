d97a787a1b339f8afca2b1fabbe139ac94de5f55
Melissa Green <melissa@sonjara.com>
2010-11-11 03:49:07 +0000

    Made dropdown menus keyboard navigable.

--- fakoli/js/menu.js
+++ fakoli/js/menu.js
@@ -1,4 +1,4 @@
-window.addEvent('domready', function()
+/*window.addEvent('domready', function()
 {
 	$$("#global > ul > li").each(function (elt)
 	{
@@ -6,4 +6,52 @@ window.addEvent('domready', function()
 					   'mouseout': function() { elt.removeClass("sfhover"); } 
 					 });
 	});
+});*/
+
+var FakoliMenu = new Class({
+	
+	root:	Class.Empty,
+	
+	initialize: function(elt)
+	{
+		this.root = $(elt);
+		var menu = this;
+		$$("#" + this.root.id + " > ul > li > a").each(function(elt) 
+		{
+			elt.addEvent('focus', function(e) 
+			{
+				var event = new Event(e).stop();
+				this.updateFocus(event, elt);
+			}.bind(menu));
+		});
+	},
+	
+	updateFocus: function(event, elt)
+	{
+		this.clearFocus();
+		
+		var parent = elt.getParent();
+		var target = event.event.target;
+		var found = false;
+		do
+		{
+			if (target == parent)
+			{
+				found = true;
+				break;
+			}
+			target = target.getParent();
+		}
+		while(target);
+		
+		if (found)
+		{
+			parent.addClass('sfhover');
+		}
+	},
+	
+	clearFocus: function()
+	{
+		this.root.getElements("ul > li").each(function(elt) { elt.removeClass('sfhover'); });
+	}
 });
