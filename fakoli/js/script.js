af657263334be0e726576c22e4cf425c0c7dbf82
Janice Gallant <janice@sonjara.com>
2011-04-28 14:58:29 +0000

    fakoli/css/image.css - add styles #image_grid, #images_box, #container #image_gallery_detail, #container #image_detail
    fakoli/js/script.js - add functions toggleArticlePublished, toggleGalleryPublished, and toggleBlogPublished

--- fakoli/js/script.js
+++ fakoli/js/script.js
@@ -29,3 +29,94 @@ function hideContextHelp(eltName)
 	if (elt == null) return;
 	elt.style.display = 'none';
 }
+
+
+function toggleArticlePublished(img, id)
+{
+	var enable;
+	if (img.alt == "Published")
+	{
+		publish = 0;
+	}
+	else
+	{
+		publish = 1;
+	}
+	
+	var request = new Request({
+									url: "/action/article/update?article_id=" + id +"&published=" + publish,
+									method: 'get',
+									onSuccess: function (response) 
+									{ 
+										if (response == 1) 
+										{
+											img.src = publish ? "/fakoli/images/on.png" : "/fakoli/images/off.png";
+											img.alt = publish ? "Published" : "Unpublished";
+										}
+									},
+									onFailure: function () {},
+									async: true
+								});
+	request.send();
+}
+
+
+
+function toggleGalleryPublished(img, id)
+{
+	var enable;
+	if (img.alt == "Published")
+	{
+		publish = 0;
+	}
+	else
+	{
+		publish = 1;
+	}
+	
+	var request = new Request({
+									url: "/action/image/update?gallery_id=" + id +"&published=" + publish,
+									method: 'get',
+									onSuccess: function (response) 
+									{ 
+										if (response == 1) 
+										{
+											img.src = publish ? "/fakoli/images/on.png" : "/fakoli/images/off.png";
+											img.alt = publish ? "Published" : "Unpublished";
+										}
+									},
+									onFailure: function () {},
+									async: true
+								});
+	request.send();
+}
+
+
+function toggleBlogPublished(img, id)
+{
+	var enable;
+	if (img.alt == "Published")
+	{
+		publish = 0;
+	}
+	else
+	{
+		publish = 1;
+	}
+	
+	var request = new Request({
+									url: "/action/blog/update?blog_id=" + id +"&published=" + publish,
+									method: 'get',
+									onSuccess: function (response) 
+									{ 
+										if (response == 1) 
+										{
+											img.src = publish ? "/fakoli/images/on.png" : "/fakoli/images/off.png";
+											img.alt = publish ? "Published" : "Unpublished";
+										}
+									},
+									onFailure: function () {},
+									async: true
+								});
+	request.send();
+}
