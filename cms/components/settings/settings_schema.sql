9bfd44c622ecfffcd9f5ad1dc6a82b4e507aa6d5
Janice Gallant <janice@sonjara.com>
2011-06-27 20:09:28 +0000

    cms/component/debug - new component - allow CMS access to trace
    cms/components/email/email_manager.inc - add function scanDebugLogs
    
    cms/components/email/manifest.inc - add admin menu item "Email Debug Logs"
    
    cms/components/email/email_debug_logs.inc - new file
    
    cms/components/email/datamodel/email_debug_logs.inc - new file
    
    cms/components/email/handlers/download_email_logs.inc - new file
    
    cms/components/settings - remove setDefaults all "trace" - moved to "debug" component; add version 1.2 to change setting record component name from settings to debug

--- cms/components/settings/settings_schema.sql
+++ cms/components/settings/settings_schema.sql
@@ -10,3 +10,11 @@
 -- settings table must be created before scan
 -- END Version 1.0
 
+-- START Version 1.1
+-- 
+
+update `settings` set `component` = 'debug', `category` = '' where `name` like 'trace_%';
+ 
+-- 
+-- END Version 1.1
+
