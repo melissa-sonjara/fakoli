3963444ea59ae9051faa5ca85cdac520594f2282
Melissa Green <melissa@sonjara.com>
2012-02-21 18:23:26 +0000

    Refactored tooltip functions into a ToolTip class to allow for easier extension and customization.

--- fakoli/js/tooltip.js
+++ fakoli/js/tooltip.js
@@ -30,60 +30,126 @@
 
 *****************************************************************/
 
+var ToolTip = new Class(
+{
+	Implements: [Options],
+	options:
+	{
+		'css': 		'tooltip_box',
+		'width':	'auto',
+		'loading':	"Loading..."
+	},
+	
+	id:			'',
+	handlerURL: '',
+	div:		null,
+	
+	initialize: function(id, options)
+	{
+		this.setOptions(ToolTip.globalOptions);
+		this.setOptions(options);
+		this.id = id;
+	},
+	
+	show: function(link, evt, handlerURL)
+	{
+		ToolTip.currentLink = document.id(link);
+		
+		var event = new Event(evt).stop();
+		
+		loadTooltip = function (link, event, tip, handlerURL)
+		{
+			if (link != ToolTip.currentLink) return;
+						
+			if (tip.div == null) 
+			{
+				tip.create();
+				tip.position(event);
+			}
+			else if(tip.handlerURL == handlerURL)
+			{
+				tip.position(event);
+				return;
+			}
+			
+			tip.handlerURL = handlerURL;
+			
+			var cursor = link.getStyle('cursor');
+		
+			link.setStyle('cursor', 'progress');
+		   	
+			var request = new Request.HTML(
+		    {	 	
+		    	evalScripts: false,
+				evalResponse: false,
+				method: 'get', 
+				url: handlerURL, 
+				onSuccess: function(tree, elements, html, script) 
+				{
+		    		tip.div.set('text', '');
+					tip.div.adopt(tree);
+					tip.position(event);
+					link.setStyle('cursor', cursor);
+					$exec(script);
+				}
+			});
+			request.send();
+		};
+			
+		loadTooltip.delay(1000, null, [ToolTip.currentLink, event, this, handlerURL]);
+	},
+
+	create: function()
+	{
+		this.div = new Element('div', {'id': this.id, 'class': this.options.css});
+		this.div.set('html', this.options.loading);
+		var doc = document.body ? document.body : document.documentElement;		
+		this.div.inject(doc, 'top'); 
+	},
+	
+	position: function(event)
+	{
+		this.div.setStyles({'display':  	'block',
+							'opacity':		0,
+						    'left':	   		event.page.x + 4,
+						    'top':      	event.page.y,
+						    'position':		'absolute',
+						    'width':		this.options.width});
+		
+		this.div.fade('in');
+	},
+
+	hide: function()
+	{
+		ToolTip.currentLink = null;
+		if (this.div == null) return;
+		
+		this.div.fade('out');
+	}
+});
+
+ToolTip.globalOptions = {};
+ToolTip.tooltips = {};
+ToolTip.currentLink = null;
+
+ToolTip.getToolTip = function(id, options)
+{
+	if (!ToolTip.tooltips[id])
+	{
+		ToolTip.tooltips[id] = new ToolTip(id, options);
+	}
+	return ToolTip.tooltips[id];
+};
+
 function showToolTip(link, evt, id, handlerURL, width)
 {
-	var link = $(link);
-	currentLink = $(link);
-	
-	var event = new Event(evt).stop();
-	loadTooltip = function (link, event, id, handlerURL)
-	{
-		if (link != currentLink) return;
-		
-		var div = $(id);
-		if(div == null) 
-		{
-			div = new Element('div', {'id': 'tooltip', 'class': 'tooltip_box'});
-			var doc = document.body ? document.body : document.documentElement;
-			
-			div.inject(doc, 'top'); 
-		}
-		
-		var cursor = link.getStyle('cursor');
-	
-		link.setStyle('cursor', 'progress');
-	   	
-		var request = new Request.HTML(
-	    {	 	
-	    	evalScripts: false,
-			evalResponse: false,
-			method: 'get', 
-			url: handlerURL, 
-			onSuccess: function(tree, elements, html, script) 
-			{
-	    		div.set('text', '');
-				div.adopt(tree);
-				div.setStyles({'display':  'block',
-							   'left':	   event.page.x + 4,
-							   'top':      event.page.y,
-							   'position': 'absolute',
-							   'width':		width});
-				link.setStyle('cursor', cursor);
-				$exec(script);
-			}
-		});
-		request.send();
-	};
-		
-	loadTooltip.delay(1000, null, [link, event, id, handlerURL]);
+	var tip = ToolTip.getToolTip(id);
+	if (width) tip.options.width = width;
+	tip.show(link, evt, handlerURL);
 };
 
 function hideToolTip (id)
 {
-	currentLink = null;
-	
-	var div = $(id);
-	if (div == null) return;
-
-	div.style.display = 'none';
+	var tip = ToolTip.getToolTip(id);
+	tip.hide();
 };
