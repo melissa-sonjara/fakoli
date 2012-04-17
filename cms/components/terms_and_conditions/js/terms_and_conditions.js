e02d60b49dff520ff196f83e6df6f2c031ebd329
Melissa Green <melissa@sonjara.com>
2012-04-17 03:08:42 +0000

    Terms and Conditions acceptance handling.

---
+++ cms/components/terms_and_conditions/js/terms_and_conditions.js
@@ -0,0 +1,18 @@
+var TermsAndConditions = new Class({
+});
+
+TermsAndConditions.id = Class.Empty;
+TermsAndConditions.title = "Terms And Conditions";
+
+TermsAndConditions.showTerms = function()
+{
+	if (TermsAndConditions.id == Class.Empty) return true;
+	if (TermsAndConditions.dialog != Class.Empty) return true;
+	
+	var acceptedTerms = document.id(TermsAndConditions.id);
+	if (acceptedTerms.value) return true;
+	
+	TermsAndConditions.Dialog = modalPopup(TermsAndConditions.title, "/action/terms_and_conditions/show", 800, 500, true, false);
+
+	return false;
+};
