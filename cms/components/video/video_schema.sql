80e8805d47f9948d2248b0980ff40ca20e8a6ec9
Janice Gallant <janice@sonjara.com>
2012-06-29 20:09:06 +0000

    SeaPerch Task 575
    framework/document_handlers.inc - add flv to doc types and mime types globals
    
    cms/components/video/video_schema / upgrade manager - Add video_download table and views video_download_daily_stats and video_download_total_stats
    
    cms/components/video/admin/video_gallery_stats.inc - new page
    
    cms/components/video/datamodel/video_download.inc - new file
    
    
    cms/components/video/handlers/download.inc - new file

--- cms/components/video/video_schema.sql
+++ cms/components/video/video_schema.sql
@@ -45,3 +45,46 @@ CREATE TABLE  `video_gallery` (
 ALTER TABLE `video` ADD COLUMN `sort_order` INTEGER UNSIGNED DEFAULT 0 AFTER `video_gallery_id`;
 
 -- END Version 1.1
+
+-- START Version 1.2
+
+CREATE TABLE  `video_download` (
+  `download_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `video_id` int(10) unsigned NOT NULL,
+  `download_date` datetime NOT NULL,
+  `user_id` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`download_id`),
+  KEY `downloads_by_video` (`video_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+CREATE OR REPLACe VIEW `video_download_daily_stats` AS select
+`vd`.`video_id` AS `video_id`,
+`g`.`video_gallery_id` AS `video_gallery_id`,
+date_format(`vd`.`download_date`,'%Y-%m-%d') AS `download_date`,
+count(1) AS `download_count`
+
+from ((`video_download` `vd` join `video` `v`) join `video_gallery` `g`)
+
+where ((`vd`.`video_id` = `v`.`video_id`) and
+(`g`.`video_gallery_id` = `v`.`video_gallery_id`))
+
+group by `vd`.`video_id`,date_format(`vd`.`download_date`,'%Y-%m-%d');
+
+
+
+
+CREATE OR REPLACE VIEW `video_download_total_stats` AS select
+`vd`.`video_id` AS `video_id`,
+`g`.`video_gallery_id` AS `video_gallery_id`,
+count(1) AS `download_count`
+
+from ((`video_download` `vd` join `video` `v`) join `video_gallery` `g`)
+
+where ((`vd`.`video_id` = `v`.`video_id`) and
+(`g`.`video_gallery_id` = `v`.`video_gallery_id`))
+
+group by `vd`.`video_id`;
+
+-- END Version 1.2
