2d4385c3899f94bddd6a024bfa514c376853a5f6
Melissa Green <melissa@sonjara.com>
2012-10-31 00:00:44 +0000

    Merge from componentize_cms

--- cms/components/settings/settings_schema.sql
+++ cms/components/settings/settings_schema.sql
@@ -6,6 +6,9 @@
 -- END Version xx
 
 
+-- The initial create statement for settings is in component_schema.sql 
+-- Version 0 - Bootstrap
+
 -- START Version 1.0
 -- settings table must be created before scan
 -- END Version 1.0
@@ -18,3 +21,16 @@ update `settings` set `component` = 'debug', `category` = '' where `name` like '
 -- 
 -- END Version 1.1
 
+
+-- START Version 1.2
+
+ALTER TABLE `settings` change column `value` `value` varchar(2000) NOT NULL;
+
+-- END Version 1.2
+
+-- START Version 1.3
+
+delete from `settings` where `name` = 'google_analytics';
+ALTER TABLE `settings` change column `value` `value` varchar(500) NOT NULL;
+
+-- END Version 1.3
