9e75d1fabcc944741aa0be53a22c52423bd4dbbe
Melissa Green <melissa@sonjara.com>
2013-01-30 04:43:53 +0000

    New multi-select dropdown control.

---
+++ fakoli/js/multi_select.js
@@ -0,0 +1,94 @@
+var MultiSelect = new Class(
+{
+	Implements: [Options, Events],
+	
+	container: Class.Empty,
+	dropdown:  Class.Empty,
+	
+	options:
+	{
+		message:  "Click to Select",
+		maxWidth: "400px"
+	},
+	
+	initialize: function(container, options)
+	{
+		this.container = document.id(container);
+		this.setOptions(options);
+		
+		this.container.multiSelect = this; // back reference
+		
+		this.buildDropdown();
+		this.selectionChanged();
+	},
+		
+	buildDropdown: function()
+	{
+		var p = this.container.getParent();
+		
+		this.dropdown = new Element('a', 
+		{
+			'class': 'multi_select_dropdown',
+			'href': '#',
+			'text': this.options.message
+		});
+		
+		this.dropdown.setStyle("max-width", this.options.maxWidth);
+		
+		this.dropdown.inject(p, 'top');
+
+
+		this.container.setStyles({display: 'none', opacity: 0});
+		
+		this.container.addEvent('mouseleave', function(e) { this.container.fade('out'); }.bind(this));
+			
+		this.dropdown.addEvent('click', function(e)
+		{
+			new Event(e).stop();
+			if (this.container.getStyle('display') == 'none' || this.container.getStyle('opacity') == 0)
+			{
+				$$('.multi_select_dropdown_list').each(function(elt) { elt.setStyle('display', 'none');});
+				this.container.setStyles({display: 'block', opacity: 0});
+				this.container.position({'relativeTo': this.dropdown, 'position': 'bottomLeft', 'edge': 'topLeft'});
+				this.container.fade('in');
+			}
+			else
+			{
+				this.container.fade('out');
+			}
+		}.bind(this));
+		
+		this.container.getElements('input[type=checkbox]').each(function(cbox)
+		{
+			cbox.addEvent('click', function(e) { this.selectionChanged(); }.bind(this));
+		}.bind(this));
+		
+	},
+	
+	selectionChanged: function()
+	{
+		var selected = this.container.getElements('input[type=checkbox]');
+		
+		var message = [];
+		var self = this;
+		selected.each(function(cbox)
+		{
+			if (cbox.checked)
+			{
+				var label = self.container.getElement('label[for="' + cbox.id + '"]');
+				if (label)
+				message.push(label.get('text'));
+			}
+		});
+
+		if (message.length == 0)
+		{
+			this.dropdown.set('text', this.options.message);
+		}
+		else
+		{
+			this.dropdown.set('text', message.join(", "));			
+		}
+	}
+});
+
