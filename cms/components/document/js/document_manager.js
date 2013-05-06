7df62661151358048e60b9ecded11877fec5cdde
Janice Gallant <janice@sonjara.com>
2013-05-06 18:11:55 +0000

    Change document library display to data list view rather than tree control and use taxonomies

---
+++ cms/components/document/js/document_manager.js
@@ -0,0 +1,100 @@
+/**
+ * Handles document library interface.
+ */
+var DocumentManager =  new Class
+({
+	dialog: null,
+	document_library_id: null,
+	
+	initialize: function(document_library_id)
+	{
+		this.document_library_id = document_library_id;
+	},
+	
+	editDocumentDetailsDialog: function(document_id)
+	{
+		this.dialog = modalPopup('Edit Document Details', '/action/document/edit?document_id=' + document_id, '550px', 'auto', true);
+	},
+	
+	showDocumentDetailsDialog: function(document_id)
+	{
+		this.dialog = modalPopup('Document Details', '/action/document/document_details_dialog?document_id=' + document_id, '550px', 'auto', true);		
+	},
+	
+	editDocumentDetailsResult: function(response)
+	{
+		if(response == "OK")
+		{
+			this.closeDialog();
+			window.location.reload();
+		}
+		else
+		{
+			var err = $('Document_form__error');
+			err.set('html', response);
+			err.setStyle('display', 'table-cell');
+		}		
+	},
+	
+	closeEditDocumentDetails: function()
+	{
+		this.dialog.hide();		
+	},
+	
+	deleteFile: function(document_id)
+	{
+		if (confirm("Are you sure you want to delete this file?"))
+		{
+			
+			var request = new Request(
+					{'url': 		'/action/document/delete?document_id=' + document_id, 
+					  'method': 	'get',
+					 'onSuccess': function(response) 
+				{ 
+				if (response == "1") 
+				{
+					window.location.reload();
+				}
+			 },
+			
+			 'onFailure':	function() { alert("Failed to communicate with server");}});
+			
+			request.send();				
+		}
+		
+	},
+	
+	uploadFile: function()
+	{
+		this.dialog = modalPopup('Upload File', '/action/document/upload?document_library_id=' + this.document_library_id + '&folder_select=0', '550px', 'auto', true);
+	},
+
+	rescanLibrary: function()
+	{
+		var request = new Request(
+		{
+			'url': 		'/action/document/rescan?document_library_id=' + this.document_library_id, 
+			'method': 	'get',
+			'onSuccess': function(response) 
+			{ 
+			if (response == "OK") 
+			{
+				window.location.reload();
+			}
+			else
+			{
+				alert(result);
+			}	
+		 },
+		
+		 'onFailure':	function() { alert("Failed to communicate with server");}});
+		
+		request.send();
+	},
+	
+	closeDialog: function()
+	{
+		this.dialog.hide();
+	}
+
+});
