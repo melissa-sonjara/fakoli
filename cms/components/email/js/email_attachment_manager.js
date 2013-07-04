046f1ace6fe30614af2375a94c68e298f9ae98bb
Janice Gallant <janice@sonjara.com>
2013-07-04 15:43:00 +0000

    Add class EmailAttachmentFieldRenderer to allow emails to have file attachments; modify EmailHandler to use file display name (key of attachments array) to display the file name in the attachment, if different from the file's base name; add module contact_us_att_form as contact us module with attachments

--- cms/components/email/js/email_attachment_manager.js
+++ cms/components/email/js/email_attachment_manager.js
@@ -1,52 +1,95 @@
 /**
- * Handles users adding attachments from the document library to an
- * email form.
+ * Handles users adding uploaded files to an email form.
  */
 var EmailAttachmentManager =  new Class
 ({
 	dialog: null,	
-
+	uploadDialog: Class.Empty,
+	form: Class.Empty,
+	list: Class.Empty,
+	cssClass: "",
+	deleteIcon: "",
+	idTagName: null,
+	classVarName: null,
 	
 	initialize: function(list, control, cssClass, deleteIcon)
 	{
+		this.form = document.id('attachmentForm');
 		this.list = document.id(list);
 		this.control = document.id(control);
 		this.cssClass = cssClass;
 		this.deleteIcon = deleteIcon;
-	},
+		
+		this.idTagName = "attachment";
+		this.classVarName = "emailAttachmentMgr";
 	
-	showDocumentSelectDialog: function()
-	{
-		this.dialog = modalPopup("Select Document from a Library", "/action/email/document_select_dialog", '600px', 'auto', true);
-	},
-	
-	documentAttachmentResult: function(response)
-	{
-		if (response.indexOf("OK") == 0)
+		// empty for document library
+		if(this.form)
 		{
-			var responseFields = response.split("|");
-			var id = responseFields[1];
-			var library_name = responseFields[2];
-			var name = responseFields[3];
-			var file_name = responseFields[4];
-			var icon = responseFields[5];
-			var size = responseFields[6];
+			this.formSetup();
+		}
+	},
+	
+	formSetup: function()
+	{
+		this.form.iFrameFormRequest(
+		{
+			'onRequest': function() { this.postStart(); return true; }.bind(this),
+			'onComplete': function(response) { this.postComplete(response);}.bind(this)
+		});	
+		
+		this.uploadDialog = new ModalDialog(document.id("attachmentDialog"), {draggable: false, closeLink: 'closeAttachmentDialog'});	
+	},
+	
+	showAddAttachmentDialog: function()
+	{
+		this.form.reset();
+		document.id('attachmentDialogMessage').innerHTML = "";
+		this.uploadDialog.show();
+	},
+	
+	postStart: function()
+	{
+		document.id('attachmentDialogMessage').innerHTML = "Uploading file...";
+	},
+	
+	postComplete: function(response)
+	{
+		var match = response.match(/^(.*?):(.*?):(.*?):(.*)$/);
+		
+		if (match)
+		{
+			var id = match[1];
+			var name = match[2];
+			var icon = match[3];
+			var size = match[4];
 			
-			this.list.innerHTML += 
-				"<li id='document_" +id + "' class='" + this.cssClass + "'><span><img src='" + icon + "' alt='Icon' style='display: inline-block;vertical-align: middle'/>&nbsp;" +
-				library_name + "&nbsp;" + name + "<br>" + file_name + "&nbsp;(" + size + ")&nbsp;" +
-				"<a href='#' onclick='emailAttachmentMgr.removeAttachment(\"" + name + "\", " + id + "); return false' title='Remove this Attachment'>" +
-				"<img src='" + this.deleteIcon + "' style='display:inline-block; vertical-align: middle' alt='Remove this Attachment'/></a></span></li>";
+			this.list.innerHTML += this.formatFilename(id, icon, name, name, size);
+		
+			this.setControlValue(id);
 			
-			if (this.control.value) this.control.value += ",";
-			this.control.value += id;
-			
-			this.closeDialog();
+			this.uploadDialog.hide();
 		}
 		else
 		{
-			$("DocumentSelect_form__error").set('html', response);
-		}		
+			document.id('attachmentDialogMessage').innerHTML = response;
+		}
+	},
+	
+	formatFilename: function(id, icon, name, display_name, size)
+	{
+		var html = "<li id='" + this.idTagName + '_' + id + "' class='" + this.cssClass + "'><span><img src='" + icon + "' alt='Icon' style='display: inline-block;vertical-align: middle'/>&nbsp;" +
+		display_name + "&nbsp;(" + size + ")&nbsp;" +
+		"<a href='#' onclick='" + this.classVarName + ".removeAttachment(\"" + name + "\", " + id + "); return false' title='Remove this Attachment'>" +
+		"<img src='" + this.deleteIcon + "' style='display:inline-block; vertical-align: middle' alt='Remove this Attachment'/></a></span></li>";
+	
+		return html;
+	},
+	
+	setControlValue: function(id)
+	{
+		if (this.control.value) this.control.value += ",";
+		this.control.value += id;	
 	},
 	
 	/**
@@ -61,11 +104,7 @@ var EmailAttachmentManager =  new Class
 	{
 		if (!confirm("Are you sure you want to remove " + filename + "?")) return;
 		
-		var elt = $('document_' + id);
-		if(elt)
-		{
-			elt.dispose();
-		}
+		this.disposeElt(id);
 		
 		if (this.control.value)
 		{
@@ -79,8 +118,63 @@ var EmailAttachmentManager =  new Class
 		}
 	},	
 	
+	disposeElt: function(id)
+	{
+		var elt = $(this.idTagName + '_' + id);
+		if(elt)
+		{
+			elt.dispose();
+		}	
+	},
+	
 	closeDialog: function()
 	{
 		this.dialog.hide();
+	}
+}); // end EmailAttachmentManager
+
+
+/**
+ * Handles users adding attachments from the document library or
+ * to an email form.
+ */
+var EmailDocumentAttachmentManager =  new Class
+({
+	Extends: EmailAttachmentManager,
+	
+	initialize: function(list, control, cssClass, deleteIcon)
+	{
+		this.parent(list, control, cssClass, deleteIcon);
+		this.idTagName = "attachment";	
+		this.classVarName = "emailDocAttachmentMgr";
+	},
+	
+	showDocumentSelectDialog: function()
+	{
+		this.dialog = modalPopup("Select Document from a Library", "/action/email/document_select_dialog", '600px', 'auto', true);
+	},
+
+	documentAttachmentResult: function(response)
+	{
+		if (response.indexOf("OK") == 0)
+		{
+			var responseFields = response.split("|");
+			var id = responseFields[1];
+			var library_name = responseFields[2];
+			var name = responseFields[3];
+			var file_name = responseFields[4];
+			var icon = responseFields[5];
+			var size = responseFields[6];
+			var display_name = library_name + "&nbsp;" + name + "<br>" + file_name;
+			
+			this.list.innerHTML += this.formatFilename(id, icon, name, display_name, size);
+			this.setControlValue(id);
+					
+			this.closeDialog();
+		}
+		else
+		{
+			$("DocumentSelect_form__error").set('html', response);
+		}		
 	},
 });
