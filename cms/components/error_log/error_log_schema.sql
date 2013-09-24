f9bd36cf427d8f1b26eee40971299d42e84dc328
Melissa Green <melissa@sonjara.com>
2013-09-24 20:29:59 +0000

    Added a date-range filter to the admin error log page.

--- cms/components/error_log/error_log_schema.sql
+++ cms/components/error_log/error_log_schema.sql
@@ -8,18 +8,18 @@
 
 -- START Version 1.0
 
-CREATE TABLE `error_log` (
-  `error_log_id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  `code` INTEGER UNSIGNED NOT NULL,
-  `message` TEXT NOT NULL,
-  `file` VARCHAR(500) NOT NULL,
-  `line` INTEGER UNSIGNED NOT NULL,
-  `details` TEXT NOT NULL,
-  `user_id` INTEGER UNSIGNED NOT NULL,
-  `uri` VARCHAR(1000) NOT NULL DEFAULT '',
-  `session` VARCHAR(200) NOT NULL DEFAULT '',
-  `timestamp` DATETIME NOT NULL,
-  PRIMARY KEY (`error_log_id`)
+CREATE TABLE error_log (
+  error_log_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  code INTEGER UNSIGNED NOT NULL,
+  message TEXT NOT NULL,
+  file VARCHAR(500) NOT NULL,
+  line INTEGER UNSIGNED NOT NULL,
+  details TEXT NOT NULL,
+  user_id INTEGER UNSIGNED NOT NULL,
+  uri VARCHAR(1000) NOT NULL DEFAULT '',
+  session VARCHAR(200) NOT NULL DEFAULT '',
+  timestamp DATETIME NOT NULL,
+  PRIMARY KEY (error_log_id)
 )
 ENGINE = InnoDB;
 
@@ -27,6 +27,14 @@ ENGINE = InnoDB;
 
 -- START Version 1.1
 
-ALTER TABLE `error_log` ADD COLUMN `referer` VARCHAR(1000) NOT NULL DEFAULT '';
+ALTER TABLE error_log ADD COLUMN referer VARCHAR(1000) NOT NULL DEFAULT '';
 
 -- END Version 1.1
+
+-- START Version 1.2
+
+ALTER TABLE fakoli_default.error_log
+ ADD INDEX error_log_session_idx(user_id, session),
+ ADD INDEX error_log_timestamp_idx(timestamp, user_id);
+
+-- END Version 1.2
