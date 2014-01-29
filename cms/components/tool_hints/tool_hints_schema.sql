9451ac872136ed9aa845247de942ad1de20751bb
Melissa Green <melissa@sonjara.com>
2014-01-29 03:19:40 +0000

    Added new tool_hints component that provides "Don't show me this again" style popup hints.

---
+++ cms/components/tool_hints/tool_hints_schema.sql
@@ -0,0 +1,32 @@
+-- Component Schema for tool_hints
+--
+-- Author: Andrew Green
+--
+-- Each version update must begin with the following:
+-- START Version xx
+-- and end with
+-- END Version xx
+
+
+-- START Version 1.0
+
+create table tool_hint
+(
+	hint_id	int(10) unsigned not null auto_increment,
+	code varchar(200) not null default '',
+	title varchar(1000) not null default '',
+	hint text null,
+	primary key(hint_id)
+) Engine=InnoDB;
+
+
+create table tool_hint_user_status
+(
+	status_id int(10) unsigned  not null auto_increment,
+	hint_id	int(10) unsigned  not null,
+	user_id int(10) unsigned  not null,
+	hidden tinyint(3) not null default 0,
+	primary key(status_id)
+) Engine=InnoDB;
+
+-- END Version 1.0
