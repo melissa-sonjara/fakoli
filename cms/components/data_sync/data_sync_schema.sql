3cf5a0ef3ba27b3c6d18369256ad7eabfde58df1
Melissa Green <melissa@sonjara.com>
2014-03-03 21:48:11 +0000

    Added grouping flag to data_import_field_mapping, to specify that a column exhibits 'grouping behavior'. If set to true, when the field is blank for a given row, the previous value of that field is used.

--- cms/components/data_sync/data_sync_schema.sql
+++ cms/components/data_sync/data_sync_schema.sql
@@ -9,19 +9,25 @@
 
 -- START Version 1.0
 
-CREATE TABLE `data_import_field_mapping` (
-	`mapping_id` int(11) NOT NULL AUTO_INCREMENT,
-	`class` VARCHAR(255) NOT NULL,
-	`client_field` VARCHAR(255) NOT NULL,
-	`import_column` VARCHAR(255) NULL,
-	`matching` TINYINT(3) UNSIGNED NOT NULL DEFAULT 0,
-	PRIMARY KEY (`mapping_id`)
+CREATE TABLE data_import_field_mapping (
+	mapping_id int(11) NOT NULL AUTO_INCREMENT,
+	class VARCHAR(255) NOT NULL,
+	client_field VARCHAR(255) NOT NULL,
+	import_column VARCHAR(255) NULL,
+	matching TINYINT(3) UNSIGNED NOT NULL DEFAULT 0,
+	PRIMARY KEY (mapping_id)
 ) ENGINE=InnoDB;
 
 -- END Version 1.0
 
 -- START Version 1.1
 
-ALTER TABLE `data_import_field_mapping` ADD COLUMN `required` TINYINT(3) UNSIGNED NOT NULL DEFAULT 0;
+ALTER TABLE data_import_field_mapping ADD COLUMN required TINYINT(3) UNSIGNED NOT NULL DEFAULT 0;
 
 -- END Version 1.1
+
+-- START Version 1.2
+
+ALTER TABLE data_import_field_mapping ADD COLUMN grouping TINYINT(3) UNSIGNED NOT NULL DEFAULT 0;
+
+-- END Version 1.2
