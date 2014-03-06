fbeb9c805f85f4f7f143ced2153cff9547f33ede
Melissa Green <melissa@sonjara.com>
2014-03-06 16:52:44 +0000

    Added home pages and priorities to site roles. The priority dictates which home page is used if a user has multiple roles assigned.

--- cms/components/role/role_schema.sql
+++ cms/components/role/role_schema.sql
@@ -9,3 +9,10 @@
 -- START Version 1.0
 -- create role table and insert record before scan
 -- END Version 1.0
+
+-- START Version 1.1
+
+ALTER TABLE site_role ADD COLUMN priority int(10) not null default 1;
+ALTER TABLE site_role ADD COLUMN home_page VARCHAR(500) not null default '';
+
+-- END Version 1.1
