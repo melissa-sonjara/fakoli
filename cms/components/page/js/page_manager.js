92bbc6d3893cb85579f6e50986363599b68bcbd6
Melissa Green <melissa@sonjara.com>
2014-05-13 03:11:27 +0000

    Added infrastructure to support in-page content editing. Implemented an in-page edit dialog for CMS pages. Implementation for other components to follow.

---
+++ cms/components/page/js/page_manager.js
@@ -0,0 +1,47 @@
+var PageManager = (function()
+{
+	var PageManagerSingleton = new Class(
+	{
+		dialog: null,
+		document_library_id: null,
+		
+		initialize: function()
+		{
+		},
+		
+		editPage: function(page_id)
+		{
+			this.dialog = modalPopup('Edit Page Details', '/action/page/edit?page_id=' + page_id, '900px', 'auto', true);
+		},
+		
+		editResult: function(result)
+		{
+			if (result == "OK")
+			{
+				this.closeDialog();
+				window.location.reload();
+			}
+			else if (result == "DELETED")
+			{
+				window.location.href = "/index";
+			}
+			else
+			{
+				$('Page_form__error').set({'text': result, 'display': 'table-cell'});
+			}
+		},
+		
+		closeDialog: function()
+		{
+			this.dialog.hide();		
+		},
+		
+	});
+	
+	var instance;
+	return function()
+	{
+		return instance ? instance : instance = new PageManagerSingleton();
+	};
+	
+})();
