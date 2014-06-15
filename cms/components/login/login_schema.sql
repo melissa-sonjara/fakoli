f7512777ec1f0ca9b07f3184387b80f6e7760bcb
Melissa Green <melissa@sonjara.com>
2014-06-15 19:32:17 +0000

    Added login audit trail and account locking capabilities to the standard Fakoli login component.

--- cms/components/login/login_schema.sql
+++ cms/components/login/login_schema.sql
@@ -18,3 +18,33 @@ CREATE TABLE `login_token` (
 ) ENGINE=InnoDB;
 
 -- END Version 1.0
+
+-- START Version 1.1
+
+CREATE TABLE login_attempt
+(
+	login_attempt_id	int(10) not null auto_increment,
+	username			varchar(200) not null default '',
+	ip_address			varchar(50) not null,
+	date_received		datetime not null,
+	login_mode			varchar(20) not null,
+	login_token			varchar(200) not null default '',
+	result				varchar(20) not null,
+	PRIMARY KEY(login_attempt_id)
+) ENGINE=InnoDB;
+
+-- END Version 1.1
+
+-- START Version 1.2
+
+CREATE TABLE login_account_lock
+(
+	lock_id			int(10) not null auto_increment,
+	username		varchar(200) not null,
+	lock_date		datetime not null,
+	locked_until	datetime not null,
+	active			tinyint(3) not null default 1,
+	PRIMARY KEY(lock_id)
+) ENGINE=InnoDB;
+
+-- END Version 1.2
