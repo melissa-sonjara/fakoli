d30c6e5714b22a1566c4a7568022dfb62b4c773e
Melissa Green <melissa@sonjara.com>
2014-08-08 18:54:34 +0000

    Added an enabled flag to TextLookup objects. By default they are enabled - unclick to hide any TextLookup when it is not required.

--- cms/components/text_lookup/text_lookup_schema.sql
+++ cms/components/text_lookup/text_lookup_schema.sql
@@ -8,21 +8,27 @@
 
 -- START Version 1.0
 
-CREATE TABLE `text_lookup` (
-  `text_id` int(10) unsigned NOT NULL auto_increment,
-  `code` varchar(45) NOT NULL,
-  `text` text NOT NULL,
-  `class_name` varchar(50) NOT NULL COMMENT 'class name that can access merge code values, if used',
-  PRIMARY KEY  (`text_id`)
+CREATE TABLE text_lookup (
+  text_id int(10) unsigned NOT NULL auto_increment,
+  code varchar(45) NOT NULL,
+  text text NOT NULL,
+  class_name varchar(50) NOT NULL COMMENT 'class name that can access merge code values, if used',
+  PRIMARY KEY  (text_id)
 ) ENGINE=InnoDB;
 
-CREATE TABLE  `text_translation` (
-  `text_translation_id` int(10) unsigned NOT NULL auto_increment,
-  `text_id` int(10) unsigned NOT NULL COMMENT 'references text_lookup table',
-  `language` varchar(50) NOT NULL COMMENT 'references language table',
-  `text` text NOT NULL,
-  PRIMARY KEY  (`text_translation_id`)
+CREATE TABLE  text_translation (
+  text_translation_id int(10) unsigned NOT NULL auto_increment,
+  text_id int(10) unsigned NOT NULL COMMENT 'references text_lookup table',
+  language varchar(50) NOT NULL COMMENT 'references language table',
+  text text NOT NULL,
+  PRIMARY KEY  (text_translation_id)
 ) ENGINE=InnoDB;
 
 
 -- END Version 1.0
+
+-- START Version 1.1
+
+ALTER TABLE text_lookup ADD COLUMN enabled TINYINT(3) NOT NULL DEFAULT 1;
+
+-- END Version 1.1
