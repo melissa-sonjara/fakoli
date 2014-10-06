74c83d25b5b4622d2670217249d8f3b065810e18
Melissa Green <melissa@sonjara.com>
2014-10-06 16:48:43 +0000

    RSS Feed component skeleton

---
+++ cms/components/rss_feed/rss_feed_schema.sql
@@ -0,0 +1,10 @@
+-- Component Schema for rss_feed
+--
+-- Author: Fiifi Baidoo
+--
+-- Each version update must begin with the following:
+-- START Version xx
+-- and end with
+-- END Version xx
+
+
