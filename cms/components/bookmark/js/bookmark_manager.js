3a53e97852739fa2e0900c932bba0e7735cf7f1e
Melissa Green <melissa@sonjara.com>
2015-01-09 06:27:30 +0000

    Added onComplete hook to BookmarkManager.bookmark() and BookmarkManager.editBookmark (in Javascript)

--- cms/components/bookmark/js/bookmark_manager.js
+++ cms/components/bookmark/js/bookmark_manager.js
@@ -1,13 +1,16 @@
 var BookmarkManager = new Class();
 
-BookmarkManager.bookmarkPage = function()
+BookmarkManager.bookmarkPage = function(onComplete)
 {
 	var url = new URI(document.location.href);
+	var query = url.get('query');
+	if (query) query = "?" + query;
 	
-	var uri = url.get('directory') + url.get('file') + url.get('query');
+	var uri = url.get('directory') + url.get('file') + query;
 	
 	var title = document.title;
 	
+	BookmarkManager.onComplete = onComplete;
 	BookmarkManager.bookmark_id = null;
 	BookmarkManager.popup = modalPopup("Bookmark this Page", "/action/bookmark/bookmark_popup?url=" + encodeURIComponent(uri) + "&title=" + encodeURIComponent(title), '550', 'auto', true);
 };
@@ -17,7 +20,18 @@ BookmarkManager.handleResponse = function(response)
 	if (response == "OK")
 	{
 		var module = document.id("bookmark_link");
-		module.getElement('div').set('html', "You have bookmarked this page");
+		if (module)
+		{
+			var div = module.getElement('div');
+			if (div) div.set('html', "You have bookmarked this page");
+		}
+
+		if (BookmarkManager.onComplete)
+		{
+			BookmarkManager.onComplete();
+			BookmarkManager.onComplete = false;
+		}
+		
 		if (!BookmarkManager.bookmark_id)
 		{
 			notification("This page has been added to your bookmarks");
@@ -36,8 +50,9 @@ BookmarkManager.handleResponse = function(response)
 	}
 };
 
-BookmarkManager.editBookmark = function(bookmark_id)
+BookmarkManager.editBookmark = function(bookmark_id, onComplete)
 {
 	BookmarkManager.bookmark_id = bookmark_id;
+	BookmarkManager.onComplete = onComplete;
 	BookmarkManager.popup = modalPopup("Edit Bookmark", "/action/bookmark/bookmark_popup?bookmark_id=" + bookmark_id, '550', 'auto', true);
 };
