00644c37ac3e835217a77011e5a816b043f37162
Melissa Green <melissa@sonjara.com>
2015-02-27 17:34:28 +0000

    Created a new component for assisting with open data compliance and publishing

---
+++ cms/components/open_data/open_data_schema.sql
@@ -0,0 +1,10 @@
+-- Component Schema for open_data
+--
+-- Author: Siobhan Green
+--
+-- Each version update must begin with the following:
+-- START Version xx
+-- and end with
+-- END Version xx
+
+
