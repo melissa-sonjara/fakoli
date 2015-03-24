1d9900e1b6e6b56fa45dbd39d31a6bc64c60e81e
Melissa Green <melissa@sonjara.com>
2015-03-24 16:49:07 +0000

    Added a theme definition mechanism. Components can register themselves as themes, and provide template files along with css, images and other assets. Themes can be assigned to Sites. Each Site in a multi-site Fakoli instance can have a separate theme.

--- cms/components/site/site_schema.sql
+++ cms/components/site/site_schema.sql
@@ -8,35 +8,41 @@
 
 -- START Version 1.0
 
-CREATE TABLE `site` (
-  `site_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `site_name` varchar(200) NOT NULL,
-  `domain` varchar(200) NOT NULL DEFAULT '',
-  `enabled` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  `description` varchar(300) NOT NULL DEFAULT '',
-  `default_template` varchar(200) NOT NULL,
-  `print_template` varchar(200) DEFAULT NULL,
-  `popup_template` varchar(200) DEFAULT NULL,
-  `mobile_template` varchar(200) DEFAULT NULL,
-  `home_page` varchar(200) DEFAULT NULL,
-  `default_site` tinyint(3) unsigned DEFAULT '0',
-  PRIMARY KEY (`site_id`)
+CREATE TABLE site (
+  site_id int(10) unsigned NOT NULL AUTO_INCREMENT,
+  site_name varchar(200) NOT NULL,
+  domain varchar(200) NOT NULL DEFAULT '',
+  enabled tinyint(3) unsigned NOT NULL DEFAULT '0',
+  description varchar(300) NOT NULL DEFAULT '',
+  default_template varchar(200) NOT NULL,
+  print_template varchar(200) DEFAULT NULL,
+  popup_template varchar(200) DEFAULT NULL,
+  mobile_template varchar(200) DEFAULT NULL,
+  home_page varchar(200) DEFAULT NULL,
+  default_site tinyint(3) unsigned DEFAULT '0',
+  PRIMARY KEY (site_id)
 ) ENGINE=InnoDB ROW_FORMAT=DYNAMIC;
 
 --
--- Dumping data for table `site`
+-- Dumping data for table site
 --
 
-/*!40000 ALTER TABLE `site` DISABLE KEYS */;
-INSERT INTO `site` (`site_id`,`site_name`,`domain`,`enabled`,`description`,`default_template`,`print_template`,`popup_template`,`mobile_template`,`home_page`,`default_site`) VALUES 
+/*!40000 ALTER TABLE site DISABLE KEYS */;
+INSERT INTO site (site_id,site_name,domain,enabled,description,default_template,print_template,popup_template,mobile_template,home_page,default_site) VALUES 
  (1,'Default Web Site','http://www.yourdomain.net',1,'','default.tpl','default.tpl','popup.tpl','default.tpl','/index',1);
-/*!40000 ALTER TABLE `site` ENABLE KEYS */;
+/*!40000 ALTER TABLE site ENABLE KEYS */;
 
 -- END Version 1.0
 
 -- START Version 1.1
 
-ALTER TABLE `site` ADD COLUMN `meta_tag_description` varchar(200) DEFAULT NULL;
-ALTER TABLE `site` ADD COLUMN `meta_tag_keyword` varchar(200) DEFAULT NULL;
+ALTER TABLE site ADD COLUMN meta_tag_description varchar(200) DEFAULT NULL;
+ALTER TABLE site ADD COLUMN meta_tag_keyword varchar(200) DEFAULT NULL;
 
 -- END Version 1.1
+
+-- START Version 1.2
+
+ALTER TABLE site ADD COLUMN theme VARCHAR(200) not null default '';
+
+-- END Version 1.2
