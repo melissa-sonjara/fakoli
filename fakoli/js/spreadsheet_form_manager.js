8713dd753bee216bd4c3b1adfb0fadb6b5e9f2ed
Melissa Green <melissa@sonjara.com>
2016-07-01 17:09:57 +0000

    Added SpreadsheetFormManager javascript class to simplify Javascript interactions with spreadsheet forms.

---
+++ fakoli/js/spreadsheet_form_manager.js
@@ -0,0 +1,24 @@
+/**
+ * Spreadsheet Form Manager class 
+ */
+var SpreadsheetFormManager = new Class({
+	Implements: [Options, Events],
+	spreadsheet: Class.Empty,
+
+	options:
+	{
+	},
+	
+	initialize: function(id, options)
+	{
+		this.setOptions(options);
+		this.spreadsheet = document.id(id);
+	},
+	
+	getData: function()
+	{
+		var rawData = spreadsheet.toQueryString().parseQueryString();
+		
+		//TODO: parse out into records
+	}
+});
