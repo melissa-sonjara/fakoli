590da436e6f04ee505c63f37488d96e87e9380b7
Melissa Green <melissa@sonjara.com>
2017-08-03 18:05:38 +0000

    Refactored BackgroundProcess constructor (Javascript) to make the initial request asynchronous, so as not to trigger browser warnings.

--- cms/components/process/js/background_process.js
+++ cms/components/process/js/background_process.js
@@ -23,34 +23,41 @@ var BackgroundProcess = new Class(
 		this.title = title;
 		this.setOptions(options);
 		
-		var result = httpRequest(handler);
-		
-		if (!result.match(/[0-9a-f]{32}/i))
+		var request = new Request(
 		{
-			alert(result);
-			return;
-		}
-		
-		this.id = result;
-		
-		this.dialog = modalPopup(this.title, null, this.options.width, this.options.height, true, false);
-		this.dialog.addEvent('hide', this.options.closeAction);
-		
-		this.container = new Element('div', {'class': 'process_container'});
-		this.message = new Element('div', {'class': 'process_message'});
-		this.percentage = new Element('div', {'class': 'process_percentage'});
-		this.progressBar = new Element('div', {'class': 'process_progress_bar'});
-		
-		this.progressBar.setStyle('width', 0);
-		
-		this.container.adopt(this.message);
-		this.container.adopt(this.percentage);
-		this.container.adopt(this.progressBar);
-		
-		this.dialog.options.body.adopt(this.container);
-		this.dialog.show();
-		
-		this.timer = this.updateProgress.periodical(this.options.period, this);
+			url: handler,
+			onSuccess: function(result)
+			{
+				if (!result.match(/[0-9a-f]{32}/i))
+				{
+					alert(result);
+					return;
+				}
+				
+				this.id = result;
+				
+				this.dialog = modalPopup(this.title, null, this.options.width, this.options.height, true, false);
+				this.dialog.addEvent('hide', this.options.closeAction);
+				
+				this.container = new Element('div', {'class': 'process_container'});
+				this.message = new Element('div', {'class': 'process_message'});
+				this.percentage = new Element('div', {'class': 'process_percentage'});
+				this.progressBar = new Element('div', {'class': 'process_progress_bar'});
+				
+				this.progressBar.setStyle('width', 0);
+				
+				this.container.adopt(this.message);
+				this.container.adopt(this.percentage);
+				this.container.adopt(this.progressBar);
+				
+				this.dialog.options.body.adopt(this.container);
+				this.dialog.show();
+				
+				this.timer = this.updateProgress.periodical(this.options.period, this);
+				
+			}.bind(this)
+		});
+		request.send();
 	},
 	
 	updateProgress: function()
