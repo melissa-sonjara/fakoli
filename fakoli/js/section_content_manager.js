376d2875ed58e54c6f240ad990e7d33054c78848
Melissa Green <melissa@sonjara.com>
2017-12-01 16:16:57 +0000

    Made section content dialog wider.

--- fakoli/js/section_content_manager.js
+++ fakoli/js/section_content_manager.js
@@ -12,7 +12,7 @@ var SectionContentManager = new Class(
 		this.getValues();
 		if (this.values.length == 0) return;
 
-		this.popup = modalPopup("Content Properties", "/action/section/content_properties?scope=all&" + this.getQueryParams(), '450px', 'auto', true);		
+		this.popup = modalPopup("Content Properties", "/action/section/content_properties?scope=all&" + this.getQueryParams(), '650px', 'auto', true);		
 	},
 		
 	setTemplate: function()
@@ -20,7 +20,7 @@ var SectionContentManager = new Class(
 		this.getValues();
 		if (this.values.length == 0) return;
 
-		this.popup = modalPopup("Content Properties", "/action/section/content_properties?scope=template&" + this.getQueryParams(), '450px', 'auto', true);		
+		this.popup = modalPopup("Content Properties", "/action/section/content_properties?scope=template&" + this.getQueryParams(), '650px', 'auto', true);		
 	},
 
 	setRole: function()
@@ -28,7 +28,7 @@ var SectionContentManager = new Class(
 		this.getValues();
 		if (this.values.length == 0) return;
 
-		this.popup = modalPopup("Content Properties", "/action/section/content_properties?scope=role&" + this.getQueryParams(), '450px', 'auto', true);		
+		this.popup = modalPopup("Content Properties", "/action/section/content_properties?scope=role&" + this.getQueryParams(), '650px', 'auto', true);		
 	},
 
 	setPermissions: function()
@@ -36,7 +36,7 @@ var SectionContentManager = new Class(
 		this.getValues();
 		if (this.values.length == 0) return;
 
-		this.popup = modalPopup("Content Properties", "/action/section/content_properties?scope=permissions&" + this.getQueryParams(), '450px', 'auto', true);		
+		this.popup = modalPopup("Content Properties", "/action/section/content_properties?scope=permissions&" + this.getQueryParams(), '650px', 'auto', true);		
 	},
 	
 	setSSL: function()
@@ -44,7 +44,7 @@ var SectionContentManager = new Class(
 		this.getValues();
 		if (this.values.length == 0) return;
 
-		this.popup = modalPopup("Content Properties", "/action/section/content_properties?scope=ssl&" + this.getQueryParams(), '450px', 'auto', true);		
+		this.popup = modalPopup("Content Properties", "/action/section/content_properties?scope=ssl&" + this.getQueryParams(), '650px', 'auto', true);		
 	},
 	
 	setPageTitle: function()
@@ -52,7 +52,7 @@ var SectionContentManager = new Class(
 		this.getValues();
 		if (this.values.length == 0) return;
 
-		this.popup = modalPopup("Content Properties", "/action/section/content_properties?scope=override_page_title&" + this.getQueryParams(), '450px', 'auto', true);		
+		this.popup = modalPopup("Content Properties", "/action/section/content_properties?scope=override_page_title&" + this.getQueryParams(), '650px', 'auto', true);		
 	},
 
 	setBodyClass: function()
@@ -60,7 +60,7 @@ var SectionContentManager = new Class(
 		this.getValues();
 		if (this.values.length == 0) return;
 
-		this.popup = modalPopup("Content Properties", "/action/section/content_properties?scope=body_class&" + this.getQueryParams(), '450px', 'auto', true);		
+		this.popup = modalPopup("Content Properties", "/action/section/content_properties?scope=body_class&" + this.getQueryParams(), '650px', 'auto', true);		
 	},
 	
 	cancel: function()
