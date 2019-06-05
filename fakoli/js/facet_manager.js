6dce10b3e0f42f5d022d6d090de9bcddbd2346f6
Melissa Green <melissa@sonjara.com>
2019-06-05 18:21:45 +0000

    Added CrossReferenceFacetFilter class

--- fakoli/js/facet_manager.js
+++ fakoli/js/facet_manager.js
@@ -310,3 +310,89 @@ var SelectFacetHandler = new Class(
 		return this.select.value;
 	}			
 });
+
+var CrossReferenceFacetHandler = new Class(
+{	
+	taxonomy: "",
+	select: Class.Empty,
+	checkboxes: Class.Empty,
+	manager: Class.Empty,
+	termLookup: {},
+	
+	initialize: function(name, select, manager)
+	{
+		this.name = name;
+		this.select = select;
+		this.checkboxes = this.select.getCheckboxes();
+		this.manager = manager;
+		this.manager.registerHandler(this);
+		
+		this.select.addEvent('selectionChanged', function()
+		{
+			this.manager.filterChanged();
+		}.bind(this));
+	},
+	
+	getName: function()
+	{
+		return this.taxonomy;
+	},
+	
+	preprocess: function(item)
+	{
+		var terms = item.get("data-" + this.name);
+		var id = item.get("id");
+
+		if (terms)
+		{
+			terms = terms.split(",");
+			terms.each(function(term) { this.termLookup[id + ":" + term] = true; }.bind(this));
+		}
+	},
+	
+	preprocessComplete: function()
+	{
+	},
+	
+	filter: function(item)
+	{
+		var id = item.get('id');
+		
+		for(var i = 0; i < this.checkboxes.length; ++i)
+		{
+			if (this.checkboxes[i].checked && this.termLookup[id + ":" + this.checkboxes[i].value])
+			{
+				return true;
+			}
+		}
+	},
+	
+	isClear: function()
+	{
+		for(var i = 0; i < this.checkboxes.length; ++i)
+		{
+			if (this.checkboxes[i].checked) return false;
+		}
+		
+		return true;
+	},
+	
+	getSelectedValue: function()
+	{
+		var values = "";
+		
+		for(var i = 0; i < this.checkboxes.length; ++i)
+		{
+			if (this.checkboxes[i].checked)
+			{
+				if (values != "")
+				{
+					values += ",";
+				}
+				values += this.checkboxes[i].value;
+			}
+		}
+		
+		return values;
+	}
+});
